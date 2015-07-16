134f785394c4775383bd02a6b8e644208bb73abf
Ryan Schmukler <rschmukler@gmail.com>
2015-07-16 12:12:08 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+require('source-map-support').install();
+
+require('babel/polyfill');
+require('babel/register');
+
+var gulp = require('gulp');
+
+var cache = require('gulp-cached');
+var remember = require('gulp-remember');
+var babel = require('gulp-babel');
+var plumb = require('gulp-plumber');
+var mocha = require('gulp-mocha');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['watch']);
+
+
+gulp.task('test', ['build'], function() {
+  return gulp.src('test/**/*.js')
+   .pipe(mocha());
+});
+
+gulp.task('build', function() {
+  return gulp.src(['src/*.js'])
+    .pipe(plumber())
+    .pipe(cache('js'))
+    //.pipe(sourcemaps.init())
+    .pipe(babel())
+    //.pipe(sourcemaps.write('.', { sourceRoot: 'src/' }))
+    .pipe(remember('js'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', ['build'], function() {
+  var jsWatch = gulp.watch(['src/*.js', 'test/**/*.js'], ['test'])
+    .on('change', checkRemoved('js'));
+
+ function checkRemoved(name) {
+   return function(event) {
+     if (event.type == 'deleted' && cache.caches[name][event.path]) {
+       delete cache.caches[name][event.path];
+       remember.forget(name, event.path);
+     }
+   };
+ }
+});
+
+function plumber() {
+  return plumb({
+    errorHandler: function(err) {
+      console.log(err);
+      this.emit('end');
+    }
+  });
+}
